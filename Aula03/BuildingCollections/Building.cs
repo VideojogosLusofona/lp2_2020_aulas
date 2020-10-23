2d22e1469e86be44c01c632f6e87b7ae4f376c63
Nuno Fachada <faken@fakenmc.com>
2020-10-24 00:14:24 +0100

    Adicionar exercício 7 da aula 3

---
+++ Aula03/BuildingCollections/Building.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace BuildingCollections
+{
+    public class Building : IHasValue, IComparable<IHasValue>
+    {
+        public string Type { get; }
+        public float Value { get; }
+        public float Area { get; }
+
+        public Building(string type, float  value, float area)
+        {
+            Type = type;
+            Value = value;
+            Area = area;
+        }
+
+        public override string ToString() =>
+            $"{Type,-20}{Value,8:f2}${Area,8:f2}m2";
+
+        /// <summary>
+        /// Indicates whether the current object is "equal" to another object of
+        /// the same type, according to the <see cref="IEquatable{T}"/>
+        /// interface.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// <c>true</c> if the current object is equal to the other parameter;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public bool Equals(IHasValue other)
+        {
+            if (other is null) return false;
+            return Value == other.Value;
+        }
+
+        /// <summary>
+        /// This method compares two buildings according to the
+        /// <see cref="IComparable{T}"/> interface. Buildings with a higher
+        /// value come before buildings with a lower value.
+        /// </summary>
+        /// <param name="other">
+        /// An object implementing <c>IHasValue</c> which will be compared to
+        /// the current building in terms of value.
+        /// </param>
+        /// <returns>
+        /// A value that indicates the relative order of the objects being
+        /// compared. If less than zero, the current building comes before
+        /// <paramref name="other"/>; if zero, both buildings are in the same
+        /// position; if greater than zero, the current building comes after
+        /// <paramref name="other"/>.
+        /// </returns>
+        public int CompareTo(IHasValue other)
+        {
+            // Nulls always come first in sorting
+            if (other == null) return 1;
+            if (Value < other.Value) return -1;
+            if (Value > other.Value) return 1;
+            return 0;
+        }
+    }
+}
---
+++ Aula03/BuildingCollections/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildingCollections
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Diferentes coleções de edifícios, inicialmente vazias
+            List<Building> list = new List<Building>();
+            Stack<Building> stack = new Stack<Building>();
+            Queue<Building> queue = new Queue<Building>();
+            HashSet<Building> set = new HashSet<Building>();
+
+            // Criar 4 edifícios, colocá-los num array
+            Building[] array = new Building[] {
+                new Building("Small hut", 1000f, 25f),
+                new Building("Skyscrapper", 1500f, 270.2f),
+                new Building("Cave", 90f, 45.2f),
+                new Building("Apartment", 365f, 90.5f)
+            };
+
+            // Colocar todos os edifícios em cada uma das coleções
+            foreach (Building b in array)
+            {
+                list.Add(b);
+                stack.Push(b);
+                queue.Enqueue(b);
+                set.Add(b);
+            }
+
+            // Colocar novamente o primeiro edifício em cada uma das coleções
+            list.Add(array[0]);
+            stack.Push(array[0]);
+            queue.Enqueue(array[0]);
+            set.Add(array[0]);
+
+            // Mostrar os edifícios na lista
+            Console.WriteLine("== Lista == ");
+            foreach (Building b in list) Console.WriteLine($"\t{b}");
+
+            // Mostrar os edifícios na stack
+            Console.WriteLine("== Stack == ");
+            foreach (Building b in stack) Console.WriteLine($"\t{b}");
+
+            // Mostrar os edifícios na queue
+            Console.WriteLine("== Queue == ");
+            foreach (Building b in queue) Console.WriteLine($"\t{b}");
+
+            // Mostrar os edifícios no hash set
+            Console.WriteLine("== Hash Set == ");
+            foreach (Building b in set) Console.WriteLine($"\t{b}");
+        }
+    }
+}
